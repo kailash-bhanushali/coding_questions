208939dd6f6074d738028f6e94334fbd5508c616
kally162 <kailash.b2203@gmail.com>
2021-01-18 23:16:06 -0600

    rotation gcd logic correction

--- GeeksforGeeks/main.go
+++ GeeksforGeeks/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	arr := []int{1, 2, 3, 4, 5, 6, 7}
+	arr := []int{1, 2, 3, 4, 5, 6}
 	d := 3
 	fmt.Println(rotationArray.Rotation_Array(arr, d))
 }
--- GeeksforGeeks/rotationArray/RotationArray.go
+++ GeeksforGeeks/rotationArray/RotationArray.go
@@ -2,12 +2,8 @@ package rotationArray
 
 func CalculateGCD(len, d int) int {
 	val := 1
-	temp := len
 	if len > 1 && d > 1 {
-		if len > d {
-			temp = d
-		}
-		for i := temp; i > 1; i-- {
+		for i := d; i > 1; i-- {
 			if len%i == 0 && d%i == 0 {
 				val = i
 				break
@@ -18,22 +14,20 @@ func CalculateGCD(len, d int) int {
 }
 func Rotation_Array(arr []int, d int) []int {
 	modFind := CalculateGCD(len(arr), d)
-	if modFind > 1 {
-		for i := 0; i < modFind; i++ {
-			temp := arr[i]
-			j := i
-			counter := 0
-			for counter < modFind {
-				arr[j] = arr[j+modFind]
-				j += modFind
-				counter++
-			}
-			arr[j] = temp
+	for i := 0; i < modFind; i++ {
+		temp := arr[i]
+		j := i
+		counter := 0
+		maxInnerLoop := (len(arr) / d) - 1
+		if modFind == 1 {
+			maxInnerLoop = len(arr) - 1
 		}
-	} else {
-		newarr := arr[0:d]
-		arr = arr[d:len(arr)]
-		arr = append(arr, newarr...)
+		for counter < maxInnerLoop {
+			arr[j] = arr[j+modFind]
+			j += modFind
+			counter++
+		}
+		arr[j] = temp
 	}
 	return arr
 }

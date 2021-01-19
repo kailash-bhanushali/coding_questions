23da24330eff2a0f16621759ca8b1a9bd17d47db
kally162 <kailash.b2203@gmail.com>
2021-01-18 21:22:17 -0600

    gcd implementation incomplete completed

--- GeeksforGeeks/main.go
+++ GeeksforGeeks/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+	arr := []int{1, 2, 3, 4, 5, 6, 7}
 	d := 3
 	fmt.Println(rotationArray.Rotation_Array(arr, d))
 }
--- GeeksforGeeks/rotationArray/RotationArray.go
+++ GeeksforGeeks/rotationArray/RotationArray.go
@@ -1,17 +1,39 @@
 package rotationArray
 
-func Rotation_Array(arr []int, d int) []int {
-	modFind := len(arr) / d
-	for i := 0; i < modFind-1; i++ {
-		temp := arr[i]
-		j := i
-		counter := 0
-		for counter < modFind-1 {
-			arr[j] = arr[j+modFind-1]
-			j += modFind - 1
-			counter++
+func CalculateGCD(len, d int) int {
+	val := 1
+	temp := len
+	if len > 1 && d > 1 {
+		if len > d {
+			temp = d
 		}
-		arr[j] = temp
+		for i := temp; i > 1; i-- {
+			if len%i == 0 && d%i == 0 {
+				val = i
+				break
+			}
+		}
+	}
+	return val
+}
+func Rotation_Array(arr []int, d int) []int {
+	modFind := CalculateGCD(len(arr), d)
+	if modFind > 1 {
+		for i := 0; i < modFind; i++ {
+			temp := arr[i]
+			j := i
+			counter := 0
+			for counter < modFind {
+				arr[j] = arr[j+modFind]
+				j += modFind
+				counter++
+			}
+			arr[j] = temp
+		}
+	} else {
+		newarr := arr[0:d]
+		arr = arr[d:len(arr)]
+		arr = append(arr, newarr...)
 	}
 	return arr
 }
